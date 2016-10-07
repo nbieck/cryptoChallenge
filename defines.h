cb14b42fff4d9ef51c0e93eb5c2063132aefd8d9
Niklas Bieck <niklas.bieck@digipen.edu>
2016-10-07 23:01:29 +0200

    Add a file of common definitions and add the file that will contain the encoding and decoding functionality

---
+++ defines.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+using byte = unsigned char;
+
+template <typename T>
+using vector = std::vector<T>;
+
+using string = std::string;
---
+++ encoding.cpp
@@ -0,0 +1,6 @@
+#include "defines.h"
+
+// decode a hex string to raw bytes
+vector<byte> hexToRawBytes(const string& input)
+{
+}
